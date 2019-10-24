849d8b57cb5aaeef144fe8ed4224318459c4ff1f
chieh890414 <55218623+chieh890414@users.noreply.github.com>
2019-10-24 10:11:59 +0800

    2019/10/24

---
+++ UnityProject/Assets/ani.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ani : MonoBehaviour {
+    private Animator ABC;
+
+    // Use this for initialization
+    void Start()
+    {
+        ABC = gameObject.GetComponent<Animator>();
+    }
+
+ 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            ABC.SetTrigger("吃飯觸發器");
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            ABC.SetBool("跑步開關", true);
+        }
+        else
+        {
+            ABC.SetBool("跑步開關", false);
+        }
+    }
+}
+
